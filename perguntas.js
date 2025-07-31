2e1b1d2b602b68a81aaccd3b13d168bab088a184
Kamilabiernaski <kamila.biernaski@escola.pr.gov.br>
2025-07-31 11:06:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+const perguntas = [
+{
+categoria: "[Seu dia]",
+pergunta: "[Como foi o seu dia hoje? Teve algum momento especial?]",
+resposta: "[ Meu dia foi bem tranquilo, consegui fazer tudo que tinha planejado e ainda reservei um tempinho para relaxar.]"
+},
+{
+categoria: "[O que você fez]",
+pergunta: "[O que você fez de mais interessante ou divertido hoje?]",
+resposta: "[O momento mais divertido foi quando encontrei um amigo que não via há tempos, foi ótimo colocar a conversa em dia!]"
+},
+{
+categoria: "[Felicidade]",
+pergunta: "[Como você está se sentindo agora após o seu dia?]",
+resposta: "[Estou me sentindo bem, um pouco cansado, mas feliz com o que consegui realizar hoje.]"
+},
+{
+categoria: "[Gratidão]",
+pergunta: "[Teve alguma coisa que te deixou especialmente feliz ou grato hoje?]",
+resposta: "[Sim, fiquei muito feliz por ter conseguido terminar um projeto importante no trabalho, isso me deixou bastante satisfeito.]"
+},
+{
+categoria: "[Coisas para amanhã]",
+pergunta: "[Tem alguma coisa que você gostaria de fazer amanhã para tornar o seu dia ainda melhor?]",
+resposta: "[Acho que amanhã quero começar o dia com uma caminhada e talvez experimentar uma receita nova na cozinha.]"
+}
+];
